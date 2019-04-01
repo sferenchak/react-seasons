43c23f38d0617c931c617347d08728e6075bf6b9
Steve Ferenchak <steveferenchak@gmail.com>
2019-04-01 14:44:18 -0700

    56.) Handle Error Messages Gracefully

--- src/index.js
+++ src/index.js
@@ -6,19 +6,28 @@ class App extends Component {
 		super(props);
 
 		this.state = {
-			lat: null
+			lat: null,
+			errorMessage: ''
 		};
 
 		window.navigator.geolocation.getCurrentPosition(
 			position => {
 				this.setState({ lat: position.coords.latitude });
 			},
-			err => console.log(err)
+			err => {
+				this.setState({ errorMessage: err.message });
+			}
 		);
 	}
 
 	render() {
-		return <div>Latitude: {this.state.lat}</div>;
+		return (
+			<div>
+				Latitude: {this.state.lat}
+				<br />
+				Error: {this.state.errorMessage}
+			</div>
+		);
 	}
 }
 
