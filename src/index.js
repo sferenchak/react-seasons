5320dc7d78c67b97a62b956f974dc2daf771f468
Steve Ferenchak <steveferenchak@gmail.com>
2019-04-01 14:20:58 -0700

    53.) Initialize State through Constructor

--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => {
-	window.navigator.geolocation.getCurrentPosition(
-		position => console.log(position),
-		err => console.log(err)
-	);
+class App extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			lat: null
+		};
+	}
 
-	return <div>Hi there!</div>;
-};
+	render() {
+		window.navigator.geolocation.getCurrentPosition(
+			position => console.log(position),
+			err => console.log(err)
+		);
+
+		return <div>Latitude: </div>;
+	}
+}
 
 ReactDOM.render(<App />, document.querySelector('#root'));
