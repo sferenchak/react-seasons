2db0f636926d02c0f44d3b0cae7b3a5ec2b27d4f
Steve Ferenchak <steveferenchak@gmail.com>
2019-04-19 09:18:04 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/sferenchak/react-seasons/precache-manifest.b1359dd2310d0ca37726bf4fef52bbea.js"
+  "/precache-manifest.555ddcbb587531197d65105ec6e78354.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/sferenchak/react-seasons/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
