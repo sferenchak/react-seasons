530c11baf7ee68d51bbc888b1792a2b651b592fc
Steve Ferenchak <steveferenchak@gmail.com>
2019-04-19 09:51:11 -0700

    Updates

---
+++ precache-manifest.0af1b2772505608f62f55fbcacd7a4cd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "52c4c266f8f1388f9e66",
+    "url": "/react-seasons/static/js/runtime~main.7c48b65a.js"
+  },
+  {
+    "revision": "6a5502b36e1fa55ad057",
+    "url": "/react-seasons/static/js/main.a5848704.chunk.js"
+  },
+  {
+    "revision": "72c252c37735d2180df8",
+    "url": "/react-seasons/static/js/2.c2695c00.chunk.js"
+  },
+  {
+    "revision": "6a5502b36e1fa55ad057",
+    "url": "/react-seasons/static/css/main.d2337424.chunk.css"
+  },
+  {
+    "revision": "950963bf7e4a592df433caebe20aed1a",
+    "url": "/react-seasons/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.555ddcbb587531197d65105ec6e78354.js"
+  "/react-seasons/precache-manifest.0af1b2772505608f62f55fbcacd7a4cd.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/react-seasons/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
