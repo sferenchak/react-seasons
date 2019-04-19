b11a585009215c98a2bb30ec995502ae579637df
Steve Ferenchak <steveferenchak@gmail.com>
2019-04-19 09:08:39 -0700

    Updates

---
+++ precache-manifest.b1359dd2310d0ca37726bf4fef52bbea.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a1e433b2d7005990395f",
+    "url": "/sferenchak/react-seasons/static/js/runtime~main.9b0f05ea.js"
+  },
+  {
+    "revision": "6a5502b36e1fa55ad057",
+    "url": "/sferenchak/react-seasons/static/js/main.a5848704.chunk.js"
+  },
+  {
+    "revision": "72c252c37735d2180df8",
+    "url": "/sferenchak/react-seasons/static/js/2.c2695c00.chunk.js"
+  },
+  {
+    "revision": "6a5502b36e1fa55ad057",
+    "url": "/sferenchak/react-seasons/static/css/main.d2337424.chunk.css"
+  },
+  {
+    "revision": "10519b945a54e72e6fd3e97599fa237b",
+    "url": "/sferenchak/react-seasons/index.html"
+  }
+];
